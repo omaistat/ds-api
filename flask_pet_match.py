0ba04981dcb35e13e2445eafe6105391ebbd41df
omaistat <omaistat@gmail.com>
2022-01-15 17:48:31 +0100

    new api file with df_raw

--- flask_pet_match.py
+++ flask_pet_match.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import pandas as pd
 
 app = Flask(__name__)
@@ -9,3 +9,7 @@ def hello_techies():
     return "<p>Hello from TechLabs!</p>"
     
 df_raw = pd.read_csv('https://github.com/omaistat/ds-api/blob/a95d8e17d01dea724d8d7b06a05ec11ad2846adb/cats_num_sample%20_100.csv')
+
+@app.route("/raw_data/all", methods=["GET"])
+def return_all():
+    return jsonify(df_raw)
