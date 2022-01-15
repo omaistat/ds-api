282931eb6f9d51c64ac7fb17118d07012fc17300
omaistat <omaistat@gmail.com>
2022-01-15 17:44:53 +0100

    new api file with df_raw

--- flask_pet_match.py
+++ flask_pet_match.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,3 +7,5 @@ app.config["DEBUG"] = True
 @app.route("/")
 def hello_techies():
     return "<p>Hello from TechLabs!</p>"
+    
+df_raw = pd.read_csv('https://github.com/omaistat/ds-api/blob/a95d8e17d01dea724d8d7b06a05ec11ad2846adb/cats_num_sample%20_100.csv')
