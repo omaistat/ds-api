004b1f49c49d34be8cbe9617d958e9c7b7683dd1
omaistat <omaistat@gmail.com>
2022-01-15 18:14:28 +0100

    flask file again -5

--- flask_pet_match.py
+++ flask_pet_match.py
@@ -2,6 +2,9 @@ from flask import Flask, Response
 import pandas as pd
 import os
 import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,10 +13,14 @@ app.config["DEBUG"] = True
 def hello_techies():
     return "<p>Hello from TechLabs!</p>"
    
-module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "cleaned_data_num.csv")
-df_raw = pd.read_csv(file_path, sep=",")
+#module_dir = os.path.abspath(os.path.dirname(__file__))
+#file_path = os.path.join(module_dir, "cleaned_data_num.csv")
+df_raw = pd.read_excel('https://github.com/TechLabs-Berlin/wt21-pet-match/blob/main/data_set/data_cleaned/all_cleaned_num.xlsx?raw=true')
 
 @app.route("/raw_data/json", methods=["GET"])
 def return_json():
     return Response(df_raw.to_json(orient="index"), mimetype="application/json")
+    
+# Model
+
+
