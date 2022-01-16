5f8cd2e5d2e1ebee89f577493a86ace1f68ccf25
omaistat <omaistat@gmail.com>
2022-01-16 13:27:36 +0100

    changes 01

--- api-test.py
+++ api-test.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 df_raw = pd.read_csv('cleaned_data_num.csv')
 df = df_raw.set_index('id').drop(columns = 'breed')
 cats = df.iloc[:, :18]
-@app.route('/predict', methods=['POST']) # Your API endpoint URL would consist /predict
+@app.route('/predict', methods=['GET', 'POST']) # Your API endpoint URL would consist /predict
 def predict():
     if lr:
         try:
