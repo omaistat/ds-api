ab305316ccedb6f954fc0af76d31d446c3bfe1e7
omaistat <omaistat@gmail.com>
2022-01-16 15:20:14 +0100

    more files

---
+++ flask_app.py
@@ -0,0 +1,49 @@
+import os
+import json
+from flask import Flask, jsonify, request, Response
+import pandas as pd
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+wines = [
+    {"id": 0, "alcohol": 8, "quality": 10},
+    {"id": 1, "alcohol": 12, "quality": 8},
+    {"id": 2, "alcohol": 10.5, "quality": 9},
+]
+
+module_dir = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(module_dir, "winequality-white.csv")
+df = pd.read_csv(file_path, sep=";")
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, from TechLabs!</p>"
+
+
+@app.route("/api/wines/all", methods=["GET"])
+def return_all():
+    return Response(df.to_json(orient="index"), mimetype="application/json")
+
+
+@app.route("/api/wines", methods=["GET"])
+def get_wine_by_id():
+    if "id" in request.args:
+        id = int(request.args["id"])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    row = df.iloc[[id]]
+    return Response(row.to_json(orient="index"), mimetype="application/json")
+
+
+@app.route("/api/wines/filter", methods=["GET"])
+def filter_wines():
+    requested_quality = request.args.get("quality")
+    if requested_quality:
+        filtered_data = df[df.quality == int(requested_quality)]
+        return Response(
+            filtered_data.to_json(orient="index"), mimetype="application/json"
+        )
+    return []
---
+++ pet_match_model.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df_raw = pd.read_csv('cleaned_data_num.csv')
+# MODEL
+#df = df_raw.drop(columns = 'breed')
+df = df_raw.set_index('id').drop(columns = 'breed')
+X = df.iloc[:, :37]
+y = df.iloc[:, 37]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=19)
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+
+joblib.dump(lr, 'model.pkl')
+lr = joblib.load('model.pkl')
+
+adopters = df.iloc[:, 18:37]
+cats = df.iloc[:, :18]
+
+# a series with cats' IDs to connect to prediction later
+cats_ids = pd.Series(df.index)
+
+model_columns = list(adopters.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
