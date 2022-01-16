07419fa732a49542dfc1e0d7f8b6bf47962016da
omaistat <omaistat@gmail.com>
2022-01-16 15:02:17 +0100

    working api

--- api-test.py
+++ api-test.py
@@ -4,35 +4,35 @@ import joblib
 import traceback
 import pandas as pd
 import numpy as np
-import os
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "cleaned_data_num.csv")
-df_raw = pd.read_csv(file_path, sep=";")
-
+df_raw = pd.read_csv('cleaned_data_num.csv')
+df = df_raw.set_index('id').drop(columns = 'breed')
+cats = df.iloc[:, :18]
+# a series with cats' IDs to connect to prediction later
+cats_ids = pd.Series(df.index)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Techie!</p>"
+    return "<p>Hello Techie!</p>"
 
-df = df_raw.set_index('id').drop(columns = 'breed')
-cats = df.iloc[:, :18]
 @app.route('/predict', methods=['GET', 'POST']) # Your API endpoint URL would consist /predict
 def predict():
     if lr:
         try:
-            #json_ = request.json
-            #query = pd.DataFrame(json_)
-            #query = query.reindex(columns=model_columns, fill_value=0)
-            query = pd.DataFrame([[1,2,4,3,2,5,4,3,4,5,3,4,5,2,3,4,5,4,3]])
+            json_ = request.json
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+            #test prediction
+            #query = pd.DataFrame([[1,2,4,3,2,5,4,3,4,5,3,4,5,2,3,4,5,4,3]])
             query.columns = model_columns
             query_merged = cats.merge(query, how = 'cross')
-            prediction = list(lr.predict(query_merged))
-            return jsonify({'prediction': prediction})
+            prediction = pd.Series(lr.predict(query_merged))
+            output = pd.concat([cats_ids, prediction], axis = 1).set_index('id').to_json()
+            return jsonify({'output': output})
 
         except:
 
