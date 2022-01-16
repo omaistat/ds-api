d88ef6884947794cacb37bcf38ec4ceecae79c54
omaistat <omaistat@gmail.com>
2022-01-16 15:35:10 +0100

    files changed

--- flask_app.py
+++ flask_app.py
@@ -1,49 +1,55 @@
-import os
-import json
-from flask import Flask, jsonify, request, Response
+# Dependencies
+from flask import Flask, request, jsonify
+import joblib
+import traceback
 import pandas as pd
+import numpy as np
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-wines = [
-    {"id": 0, "alcohol": 8, "quality": 10},
-    {"id": 1, "alcohol": 12, "quality": 8},
-    {"id": 2, "alcohol": 10.5, "quality": 9},
-]
-
-module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "winequality-white.csv")
-df = pd.read_csv(file_path, sep=";")
-
+df_raw = pd.read_csv('cleaned_data_num.csv')
+df = df_raw.set_index('id').drop(columns = 'breed')
+cats = df.iloc[:, :18]
+# a series with cats' IDs to connect to prediction later
+cats_ids = pd.Series(df.index)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, from TechLabs!</p>"
+    return "<p>Hello Techie!</p>"
 
+@app.route('/predict', methods=['GET', 'POST']) # Your API endpoint URL would consist /predict
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+            #test prediction
+            #query = pd.DataFrame([[1,2,4,3,2,5,4,3,4,5,3,4,5,2,3,4,5,4,3]])
+            query.columns = model_columns
+            query_merged = cats.merge(query, how = 'cross')
+            prediction = pd.Series(lr.predict(query_merged))
+            output = pd.concat([cats_ids, prediction], axis = 1).set_index('id').to_json()
+            return jsonify({'output': output})
 
-@app.route("/api/wines/all", methods=["GET"])
-def return_all():
-    return Response(df.to_json(orient="index"), mimetype="application/json")
+        except:
 
-
-@app.route("/api/wines", methods=["GET"])
-def get_wine_by_id():
-    if "id" in request.args:
-        id = int(request.args["id"])
+            return jsonify({'trace': traceback.format_exc()})
     else:
-        return "Error: No id field provided. Please specify an id."
+        print ('Train the model first')
+        return ('No model here to use')
 
-    row = df.iloc[[id]]
-    return Response(row.to_json(orient="index"), mimetype="application/json")
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
 
+    lr = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
 
-@app.route("/api/wines/filter", methods=["GET"])
-def filter_wines():
-    requested_quality = request.args.get("quality")
-    if requested_quality:
-        filtered_data = df[df.quality == int(requested_quality)]
-        return Response(
-            filtered_data.to_json(orient="index"), mimetype="application/json"
-        )
-    return []
+    app.run(port=port, debug=True)
