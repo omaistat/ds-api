01edafde68e7d3733f7d0b035efbe55ecf01a63e
omaistat <omaistat@gmail.com>
2022-01-18 23:05:56 +0100

    main api file

--- flask_app.py
+++ flask_app.py
@@ -14,20 +14,45 @@ def hello_world():
     return "<p>Hello Techie!</p>"
 
 module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "cleaned_data_num.csv")
-df_raw = pd.read_csv(file_path)
+file_path_1 = os.path.join(module_dir, "cleaned_data_num.csv")
+df_raw = pd.read_csv(file_path_1)
+
+file_path_2 = os.path.join(module_dir, "model.pkl")
+lr = joblib.load(file_path_2) # Load "model.pkl"
+
+file_path_3 = os.path.join(module_dir, "model_columns.pkl")
+model_columns = joblib.load(file_path_3)
+
+file_path_4 = os.path.join(module_dir, "cats_num_sample _100.csv")
+cats_website=pd.read_csv(file_path_4)
+
+#cleaning df
 df = df_raw.set_index('id').drop(columns = 'breed')
-cats = df.iloc[:, :18]
-# a series with cats' IDs to connect to prediction later
-cats_ids = pd.Series(df.index)
 
-module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "model.pkl")
-lr = joblib.load(file_path) # Load "model.pkl"
+#cleaning cats data
+cats_website = cats_website.drop(columns = 'Other (please specify).1')
+cats_website.columns = ['id','cat_age',
+                                                      'cat_gender',
+                                                      'needs_outdoor',
+                                                      'medical_conditions',
+                                                      'behavioural_problems',
+                                                      'cat_weight',
+                                                      'likes_to_explore',
+                                                      'playful',
+                                                      'vocal',
+                                                      'picked_up',
+                                                      'timid',
+                                                      'aggressive',
+                                                      'adapts_quickly',
+                                                      'prefers_alone',
+                                                      'likes_stroke',
+                                                      'tolerant_handled',
+                                                      'friendly',
+                                                      'fearful'
+]
 
-module_dir = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(module_dir, "model_columns.pkl")
-model_columns = joblib.load(file_path)
+
+
 
 @app.route('/predict', methods=['GET', 'POST']) # Your API endpoint URL would consist /predict
 def predict():
@@ -39,7 +64,11 @@ def predict():
             #test prediction
             #query = pd.DataFrame([[1,2,4,3,2,5,4,3,4,5,3,4,5,2,3,4,5,4,3]])
             query.columns = model_columns
-            query_merged = cats.merge(query, how = 'cross')
+            query_df = pd.DataFrame()
+            query_new = query_df.append([query]*len(cats_website)).reset_index().drop(columns = ['index'])
+            query_new['key'] = 1
+            cats_website['key'] = 1
+            query_merged = cats_website.merge(query_new,on='key', how='outer').drop(columns = 'key').set_index('id')
             prediction = pd.Series(lr.predict(query_merged))
             output = pd.DataFrame(prediction).sort_values(by = 0, ascending = False).iloc[:11].drop(columns = 0).reset_index().to_json()
             return jsonify({'output': output})
